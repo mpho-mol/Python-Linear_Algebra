4f456f6faec6a66afd8ccadfcc4e3a6ad329996b
mpho-mol <143937796+mpho-mol@users.noreply.github.com>
2023-10-26 14:13:34 +0200

    Add files via upload

--- linear_algebra.py
+++ linear_algebra.py
@@ -1,22 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-#create variables 
+# Create variables
 start_val = 0
 stop_val = 100
 n_samples = 200
 
-#create vector containing values between 0-100 in intervals of 0.5
-x = np.linspace(start_val, stop_val, n_samples)
+# Create vector containing values between 0-100 in intervals of 0.5
+X = np.linspace(start_val, stop_val, n_samples)
 
 params = np.array([2, -5])
 
-#code for applying f(x) on vector x
-f_x = params[0] * x + params[1]    #adding because params[0] is -5(negative value)
+# Code for applying f(x) on vector X
+f_X = params[0] * X + params[1]    # Adding because params[0] is -5(negative value)
 
-#plot f(x)
-plt.plot(x, f_x)
+# Plot f(X)
+plt.plot(X, f_X)
 plt.title("f(x) = 2x - 5")
 plt.xlabel("X")
 plt.ylabel("Y")
 plt.tight_layout()
+plt.show()
+plt.savefig("figure.png")
